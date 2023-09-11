68ba9aac2b9fbca78487f4f8c6c1abe425559523
pattazl <pattazl@163.com>
2023-09-11 22:34:13 +0800

    完成键盘数据的保存和更新

--- http/src/records.js
+++ http/src/records.js
@@ -3,14 +3,20 @@ const sqlite3 = require('sqlite3').verbose();
 
 let strDate = dayjs(new Date()).format('YYYY-MM-DD')
 // 创建一个连接到数据库的对象
-const db = new sqlite3.Database('records.db');
-
 // 创建表格（如果不存在）
 function insertData(records) {
   return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database('records.db');
+    let tick = records['tick']
+    // 先检查是否已经有此tick数据，如果有则删除
+    db.run('delete FROM events where tick = ? ', [tick], function (err) {
+      if (err) {
+        return console.error(err.message);
+      }
+      console.log('删除tick数据条数: ', this.changes )
+    });
     // 将record 中的所有键值 插入数据
     let arr = []
-    let tick = records['tick']
     for (let k in records) {
       if (k != 'tick') {
         arr.push({
@@ -24,12 +30,13 @@ function insertData(records) {
     const placeholders = arr.map(() => '(?, ? , ? , ?)').join(', ');
     const values = arr.reduce((acc, curr) => acc.concat([curr.name, curr.count, curr.tick, strDate]), []);
     // 执行一次性插入
+    console.log('insert into sql')
     db.run(`INSERT INTO events (keyname, keycount , tick , date) VALUES ${placeholders}`, values, function (err) {
       if (err) {
-        //return console.error(err.message);
+        console.error(err.message);
         reject(err)
       }
-      //console.log(`A total of ${this.changes} rows have been inserted`);
+      console.log(`A total of ${this.changes} rows have been inserted`);
       resolve(this.changes)
     });
     // 关闭数据库连接
@@ -38,36 +45,77 @@ function insertData(records) {
 }
 
 
-function getRecords(begin,end) {
+function getRecords(begin, end) {
+  const db = new sqlite3.Database('records.db');
   let strNow = dayjs(new Date()).format('YYYY-MM-DD')
   return new Promise((resolve, reject) => {
     // 查询记录集
     let arr = [];
-    if(begin > end){
+    if (begin > end) {
       resolve(arr)
       return;
     }
     let sql = ''
-    if(begin == strNow && end == strNow)
-    {
-      sql = 'SELECT * FROM events where date = ?'
-    }else{
-      sql = 'SELECT * FROM stat where date = ?'
+    if (begin == strNow && end == strNow) {
+      sql = 'SELECT keyname, keycount, date, tick FROM events where date between ? and ?'
+    } else {
+      sql = 'SELECT keyname, keycount, date FROM stat where date between ? and ? '
     }
-    db.all(sql, [strDate], function (err, rows) {
+    db.all(sql, [begin, end], function (err, rows) {
       if (err) {
         reject(err);
       }
       // 输出记录集
       rows.forEach(function (row) {
+        let tick = ''
+        if (row.tick != null) {
+          tick = row.tick
+        }
         let keyname = row.keyname, keycount = row.keycount, date = row.date;
-        arr.push({ keyname, keycount, date });
+        arr.push({ keyname, keycount, date, tick });
       });
       resolve(arr)
     });
+    // 检查 events 是否有小于当天的数据,如果有，则需要触发转移
+    if (begin == strNow && end == strNow) {
+      db.all('SELECT tick FROM events where date<? limit 1', [strNow], function (err, rows) {
+        if (err) {
+          //reject(err);
+        }
+        // 输出记录集
+        if (rows.length > 0) {
+          setTimeout(doCleanData, 1);
+        }
+      });
+    }
     // 关闭数据库连接
     db.close();
   })
+}
+// 将 events的旧数据统计后转移到stat 表中
+function doCleanData() {
+  const db = new sqlite3.Database('records.db');
+  console.log('doCleanData')
+  let strNow = dayjs(new Date()).format('YYYY-MM-DD')
+  // 执行sql处理，插入统计数据
+  db.run(`INSERT INTO stat (keyname, keycount,date) SELECT keyname, sum(keycount), date FROM events where date < ? group by date,keyname`,
+    [strNow], function (err) {
+      if (err) {
+        return console.error(err.message);
+      }
+      let lines = this.changes
+      console.log('转移数据条数: ',lines)
+      // 删除 events 中的旧数据
+      if (lines > 0) {
+        db.run('delete FROM events where date < ? ', [strNow], function (err) {
+          if (err) {
+            return console.error(err.message);
+          }
+          console.log('删除数据条数: ', this.changes )
+        });
+      }
+    });
+  db.close();
 }
 /*
 (async ()=>{
@@ -77,5 +125,5 @@ function getRecords(begin,end) {
 */
 
 module.exports = {
-  insertData,getRecords
+  insertData, getRecords
 };
--- http/src/server.js
+++ http/src/server.js
@@ -1,177 +1,32 @@
 const WebSocket = require('ws');
 const http = require('http');
 const express = require('express')
-const {insertData,getRecords} = require('./records');
-const dayjs = require('dayjs');
-const net = require('net');
-const app = express()
+const {getRecords} = require('./records');
 const version = '1.1'
 
-var fs = require('fs')
-  , ini = require('ini')
-const iniPath = '../../showKeyBoard.ini'
-var config = ini.parse(fs.readFileSync(iniPath, 'utf-8'))
 
-var port = parseInt(config.common.serverPort)
-
-function checkPort(port) {
-  const server = net.createServer();
-  return new Promise((resolve, reject) => {
-    server.once('error', (err) => {
-      if (err.code === 'EADDRINUSE') {
-        resolve(true); // 端口被占用
-      } else {
-        reject(err); // 发生其他错误
-      }
-    });
-
-    server.once('listening', () => {
-      server.close(() => {
-        resolve(false); // 端口未被占用
-      });
-    });
-
-    server.listen(port, '127.0.0.1');
-  });
-}
-// 创建WebSocket服务器
-const server = http.createServer(app);
- //{ server } {port: 8080 }
+const { startUp , getParaFun, setParaFun , app ,exitFun ,dataFun} = require('./common');
 
 //app.use(express.text());
 app.use(express.json());
 app.use(express.static('ui'));
 // 返回全部参数信息
-app.post('/getPara', (req, res) => {
-  console.log('getPara')
-  res.send(JSON.stringify(config));
-});
+app.post('/getPara', getParaFun);
 // 写入修改的参数信息
-app.post('/setPara', (req, res) => {
-  console.log('setPara')
-  var data = req.body
-  let newConf = JSON.stringify(data)
-  if( JSON.stringify(config) != newConf)
-  {
-		console.log('write ini')
-		config = JSON.parse(newConf)
-		fs.writeFileSync(iniPath, ini.stringify(data))
-  }
-  res.send({code:200});
-});
-app.get('/', (req, res) => {
-  console.log(req.body)
-  res.send('Welcome to showKeyBoard backend service');
-});
-app.post('/exit', (req, res) => {
-  if( preData['tick']>0 )
-  {
-    // 需要保存
-    insertData(preData)
-  }
-  console.log('exit')
-  res.send('OK');
-  wss.close()
-  server.close(()=>{})  //  退出服务
-  setTimeout(function(){process.exit();},1000);
-});
-
-// 发送数据
-let preData = {}
-app.post('/data', (req, res) => {
-  var data = req.body
-  console.log(data)
-  if( preData['tick']>0 &&  data['tick']>0 &&  data['tick'] != preData['tick'] )
-  {
-    // tick不一样需要保存
-    insertData(preData)
-  }
-  preData = data;
-  //myWS.send(JSON.stringify(data) );
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(`服务器端数据更新：${JSON.stringify(data)}`);
-    }
-  });
-  // 如果 data.tick 不一样，则需要累计保存
-  res.send('OK');
-});
+app.post('/setPara', setParaFun);
+app.get('/', (req, res) => {res.send('Welcome to showKeyBoard backend service');});
+app.post('/exit',exitFun);
+app.post('/data', dataFun);
 // 获取某某天，或某个tick的数据, 参数 date 如果是当天，返回的是所有tick的清单，否则返回
 app.post('/historyData', async (req, res) => {
     let arr = await getRecords( req.body?.beginDate , req.body?.endDate )
-    res.send(JSON.stringify(data))
+    res.send(JSON.stringify(arr))
 });
 // 版本和服务判断
-app.post('/version', (req, res) => {
-  console.log('version')
-  res.send('showKeyBoardServer Version:'+ version);
-});
+app.post('/version', (req, res) => {res.send('showKeyBoardServer Version:'+ version);});
 // 监听WS 连接事件
 let wss = null
-function startWS(){
-    wss = new WebSocket.Server(  { server });
-    wss.on('connection', (ws) => {
-      // 监听消息接收事件
-      ws.on('message', (message) => {
-        console.log('Received message:', message);
 
-        // 发送消息给客户端
-        ws.send('Server received your message: ' + message);
-      });
-      // 监听断开连接事件
-      ws.on('close', () => {
-        console.log('Client disconnected');
-      });
-    });
-    wss.on('error', () => {
-        console.log('wss error');
-        wss.close() // 发现异常需要关闭重新开始
-      });
-}
-// 创建服务器函数
-function createServer() {
-  // 尝试启动服务器
-  startWS()  // 配置WS服务
-  server.listen(port,'127.0.0.1', () => {
-    console.log(`Express server正在监听端口 ${port}`);
-    writePort()
-  }).on('error', (err) => {
-    // 端口被占用时的错误处理
-    console.log('创建服务异常');
-  })
-}
 
-function writePort()
-{
-    if(port != parseInt(config.common.serverPort))
-    {
-        config.common.serverPort = port;
-        // 写入将导致参数重启
-        fs.writeFileSync(iniPath, ini.stringify(config))
-    }
-}
 //  直接启动
-(async()=>{
-	// 首先判断端口是否被占用
-	var attemptCount = 0,maxAttempts = 5
-	while(1){
-		let isPortInUse = true
-		try{
-		   isPortInUse = await checkPort(port);
-		}catch(e){}
-		if(!isPortInUse){
-			createServer()
-			break;
-		}
-		port++;
-		if (attemptCount < maxAttempts) {
-		  // 增加尝试次数并自增端口号
-		  attemptCount++;
-		  console.log(`端口 ${port - 1} 被占用，尝试使用下一个端口 ${port}...`);
-		} else {
-		  // 达到最大尝试次数，无法启动服务器
-		  console.log(`无法启动服务器，已达到最大尝试次数 ${maxAttempts}`);
-		  break;
-		}
-	}
-})()
+startUp()
