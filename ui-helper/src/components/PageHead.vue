3f11100dbda13aa48fe28c7aea210fc31f2a6186
austin.young <pattazl@163.com>
2025-07-29 17:48:29 +0800

    初步增加相关版本信息

--- ui-helper/src/components/PageHead.vue
+++ ui-helper/src/components/PageHead.vue
@@ -2,7 +2,7 @@
   <n-layout-header bordered class="nav">
     <div class="nav-box">
       <n-text tag="div" class="ui-logo" :depth="1">
-        <span>{{ contentText.title + strVersion }} {{ serverVer }}</span>
+        <span>{{ contentText.title + strVersion }} {{ serverVer }}</span> <span :style="urlColor"><a target="_blank" :href="url" :title="urlInfo" >{{ majorVersion }}</a></span>
       </n-text>
       <n-space justify="end">
         <n-button v-if="lang === 'zh-CN'" @click="onLangChange('en-US')">
@@ -59,13 +59,60 @@ export default defineComponent({
         theme.value = 'dark'
       }
     };
+    // 获取 gitee 或github上的信息
+    function getGitLatestRelease() {
+      // 循环访问
+      let gitServer = ['https://api.github.com/repos/pattazl/showKeyBoard/releases/latest',
+        'https://gitee.com/api/v5/repos/pattazl/showkeyboard/releases/latest']
+      // 获取最新版本信息， lastVer , info
+      let getLatestVerInfo = [
+        (x) => {
+          let latestVer = x.tag_name;
+          let info = x.body;
+          let url = 'https://github.com/pattazl/showkeyboard/releases/latest'
+          return { latestVer, info, url }
+        }, // 对于github 的获取方式
+        x => {
+          let latestVer = x.tag_name;
+          let info = x.body;
+          let url = 'https://gitee.com/pattazl/showkeyboard/releases/latest'
+          return { latestVer, info, url }
+        }, // 对于gitee 的获取方式
+      ]
+      let latestVerInfo = null
+      gitServer.some(async (url, index) => {
+        let rsp = await fetch(url, {
+          method: "get"
+        })
+        let result = await rsp.json();
+        let info = getLatestVerInfo[index](result)
+        if (typeof info?.latestVer == 'string') {
+          latestVerInfo = info
+          gitServerInfo.value = latestVerInfo
+          return true;
+        }
+      })
+      console.log(latestVerInfo)
+    }
+    let gitServerInfo = {}
     let serverVer = ref('');
+    let urlColor = ref('');
+    let url = ref('');
+    let urlInfo = ref('');
+    let majorVersion = ref('');
     // ajax方式获取服务器端版本号
     onMounted(async () => {
       let res = await ajax('version')
       let ver = res?.ver
       if (ver != null) {
         serverVer.value = `(Server ${ver})`
+      }
+      let mVer = res?.majorVersion
+      if (mVer != null) {
+        // 尝试获取 git上的版本信息
+        getGitLatestRelease();
+        // 判断版本号
+        majorVersion.value = mVer
       }
     })
     return {
@@ -76,6 +123,10 @@ export default defineComponent({
       contentText,
       strVersion,
       serverVer,
+      majorVersion,
+      urlColor,
+      url,
+      urlInfo,
     };
   },
 });
