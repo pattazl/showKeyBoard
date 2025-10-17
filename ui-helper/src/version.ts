4c40a17f8fd2ae72a731a413e5bb9c27901e1a6d
austin.young <pattazl@163.com>
2025-10-17 18:51:27 +0800

    增加可能导致按键延时的说明，后续考虑将显示做成独立进程，避免卡顿，也方便支持多系统。准备发布1.54版

--- ui-helper/src/version.ts
+++ ui-helper/src/version.ts
@@ -4,7 +4,7 @@
 // 对于发布版 YYMMDD 和 verNo 会在Webpack中自动替换为真实的日期和版本序号,如下格式不能随便改，参考vue.config.js的代码
 const replaceYYMMDD = '250918';
 const replaceVerNo = '1';
-const mainVersion = ' v1.51';
+const mainVersion = ' v1.54';
 const strVersion = mainVersion+'.'+replaceYYMMDD+'.'+replaceVerNo;   // 可以支持旧版浏览器
 //export default strVersion
 export {strVersion};
