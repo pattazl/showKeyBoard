6f544de47a2d7ff7982699c86e7f2f4fe15b49ac
ZzzHe <c@0u0.space>
2026-02-25 11:31:00 +0800

    Create buildMain.config.js

---
+++ build/buildMain.config.js
@@ -0,0 +1,7 @@
+// 此文件配置编译的文件路径包含 AHK2EXE 和 NSIS的
+const Ahk2Exe = 'D:\\Program Files\\AutoHotkey\\Compiler\\Ahk2Exe.exe'
+const NSIS = 'D:\\Program\\NSIS2.46\\NSIS2.46cn\\makensis.exe'
+module.exports = {
+    Ahk2Exe,
+    NSIS
+};
